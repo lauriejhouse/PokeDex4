598b53922f45a88a2e83d3f177be3ae56e1d2ac4
Unknown <jackienorstrom@aol.com>
2019-02-12 11:05:25 -0500

    Linked up local and gitkraken.

---
+++ PokeDex4/PokeDex4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeDex4
+//
+//  Created by Jackie Norstrom on 2/12/19.
+//  Copyright © 2019 LAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PokeDex4/PokeDex4Tests/PokeDex4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  PokeDex4Tests.swift
+//  PokeDex4Tests
+//
+//  Created by Jackie Norstrom on 2/12/19.
+//  Copyright © 2019 LAS. All rights reserved.
+//
+
+import XCTest
+@testable import PokeDex4
+
+class PokeDex4Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
